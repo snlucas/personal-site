873bdccfb93a271e37399b0a2f59fdcffb37d586
Lucas Nogueira <lucas.lambda.101@gmail.com>
2023-03-11 13:50:28 -0300

    Add cpfgen project

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,7 +6,20 @@ export const data = [
     image: '/images/projects/scrape_imdb.webp',
     date: '2023-03-04',
     desc:
-      'A web project that scrape and save IMDB top movies into a MySQL database. This project serves it as a web app to show top 100 movies, top 10 movies, and to fetch the database, updating its data.',
+      'A web project that scrape and save IMDB top movies into a MySQL database. '
+      + 'This project serves it as a web app to show top 100 movies, top 10 movies, '
+      + 'and to fetch the database, updating its data.',
+  },
+  {
+    title: 'CPFGen',
+    subtitle: 'A Brazilian taxpayer registry (CPF) generator.',
+    link: 'https://snlucas.github.io/cpfgen/',
+    image: '/images/projects/cpf_gen.webp',
+    date: '2023-01-31',
+    desc:
+      'An automation project that uses Selenium to capture data from a website, '
+      + 'display it in the command line, and save it to the clipboard, '
+      + 'making it easier to paste.',
   },
 ];
 
@@ -18,7 +31,21 @@ export const dataBr = [
     image: '/images/projects/scrape_imdb.webp',
     date: '2023-03-04',
     desc:
-      'Projeto web que busca dados sobre os filmes mais assistidos no site do IMDB e salva todos eles em um banco de dados MySQL. Esse projeto usa uma web app para mostrar os top 100 filmes mais assistidos, os top 10 e possui uma opção de fetch para atualizar os dados salvos.',
+      'Projeto web que busca dados sobre os filmes mais assistidos no site do IMDB e '
+      + 'salva todos eles em um banco de dados MySQL. Esse projeto usa uma web app '
+      + 'para mostrar os top 100 filmes mais assistidos, os top 10 e possui uma opção '
+      + 'de fetch para atualizar os dados salvos.',
+  },
+  {
+    title: 'CPFGen',
+    subtitle: 'Gerador de CPFs de linha de comando.',
+    link: 'https://snlucas.github.io/cpfgen/',
+    image: '/images/projects/cpf_gen.webp',
+    date: '2023-01-31',
+    desc:
+      'Projeto de automação, usando Selenium para acessar site gerador de CPFs e '
+      + 'mostrar em linha de comando. Também coloca o resultado no clipboard, tornando '
+      + 'possível colar em outro lugar.',
   },
 ];
 
